690d7c1cee22825d1fccd1807f29c41d979555e9
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-04-29 17:28:51 -0300

    Arrays, arrays asociativos y match

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ var_dump($concatenacion);
 echo gettype($año);
 
 # se pueden forzar datos con (tipo de dato)
-$edad = (int) 33;
+$age = (int) 33;
 
 #interpolación de cadenas (only "x")
 $output = "Hola tengo $edad";
@@ -75,4 +75,50 @@ if ($edad > 60){
 
 #PLANTILLAS EN PHP - sintaxis alternativa
 # PARSE ERROR -> ES EL HORROR MÁS HORRIBLE
-#¿Que es ajax?
+
+#MATCH > SWITCH
+#Compara la variable con los demás numeros para determinar la variable outputAge
+$outputAge = match($age) {
+ 0, 1, 2 => "Eres un bebe, $name",
+ 3,4,5,6,7,8,9,10 => "Eres un niño, $name",
+ 11,12,13,14,15,16,17,18 => "Eres un adolescente, $name",
+ 19,20,21,22,23,24,25,26,27,28,29,30 => "Eres un adulto, $name",
+ default => "Eres un viejo, $name", 
+};
+
+#En vez de hacer eso se pueden evaluar expresiones en vez de numeros.
+# outputAge = match (true){
+# $age < 2   => "Eres un niño"
+# $age < 10  => "Eres un niño"
+# $age < 18  => "Eres un adolescente
+# $age == 18 => "Eres mayor" 
+# $age > 18  => "Eres un adulto"
+# }
+
+#Arrays -> faciles;
+$bestLanguages = ["PHP","Javascript","Java", 1, 2, 4,];
+$bestLanguages[] = "Java";
+# No es necesario poner limites y va al final 
+$bestLanguages[2] = "C"
+
+#Iterar arrays:
+?>
+
+<ul>
+    <?php foreach($bestLanguages as $key => $language) : ?>
+        <li><?= $key . " " . $language ?></li>
+    <?php endforeach; ?>
+</ul>
+
+<?php
+#Hay varios linters para php -> herramientas que mejoran el codigo.
+# Hay arrays que serìan similares a un diccionario -> arrays asociativos.
+
+$person = [  
+    "nombre" => "Miguel",
+    "edad" => 75, 
+    "isDev" => true, 
+    "languages" => ["Php, java, python"],  
+];
+
+?>
