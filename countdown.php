e1e53b811ba3c1e70801944b339136c003f97fff
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-04-29 22:26:54 -0300

    proyecto de midudev

--- countdown.php
+++ countdown.php
@@ -20,5 +20,52 @@ curl_close($ch);
 ?>
 
 <head>
-    <t
+    <meta charset="UTF-8">
+    <meta name="description" content="La proxima pelicula de Marvel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MCU Peliculas</title>
+    <!-- Pico css  --> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/>
 </head> 
+
+<main>
+    <section>
+        <img 
+            src="<?= $data["poster_url"]; ?>" width="200" alt="poster de" <?= $data["title"] ?> 
+            stlye="border-radius: 16px"/>
+    </section>
+    <hgroup>
+        <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?> dias </h3>
+        <p>Fecha de estreno: <?= $data["release_data"]; ?> </p>
+        <p>La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
+    </hgroup>
+</main>
+
+
+<style>
+    :root{
+        color-scheme: light dark;
+    }
+
+    body{
+        display: grid;
+        place-content: center;
+    }
+
+    section{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    img{
+        margin: 0 auto;
+    }
+</style>
