7c61841f578d2350f682a4ae6d8e85995fc9be11
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-12-31 22:10:06 -0300

    final

--- templates/body.php
+++ templates/body.php
@@ -1,13 +1,26 @@
-<main>
-  <hgroup>
+<body>
+  <header>
+    <nav>
+      <h1>Presidentes, ministros y sucesos economicos desde 1990</h1>
+    </nav>
+  </header>
+  </nav>
+  <section>
     <?php 
     foreach($data as $index => $item) {
-      echo "Ítem $index:<br>";
-      echo "Fecha: " . $item["d"] . "<br>";  
+
+      $cargo = match(true) {
+        $item['t'] == 'econ' => 'Ministro de economía',
+        $item['t'] == 'pres' => 'Presidente',
+        $item['t'] == 'bcra' => 'Presidente del BCRA',
+        default => 'no especificado'
+      };
+      
+      
+      echo "<div class='items'>" . "Fecha: " . $item["d"] . "<br>";  
       echo "Nombre: " . $item["e"] . "<br>";   
-      echo "Tipo: " . $item["t"] . "<br>";     
-      echo "<hr>";
-    } 
+      echo "Tipo: " . $cargo . "<br>" ."</div>";
+    }
     ?>
-  </hgroup>
-</main>
+  </section>
+</body>
--- templates/styles.php
+++ templates/styles.php
@@ -1,8 +1,28 @@
 <style>
+
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+
+  nav{
+    background-color: gainsboro;
+    text-align: center;
+    padding: 1rem;
+  }
   
-  body {
+  section{
     display: grid;
     place-content: center;
+    grid-template-rows: (2fr, 1fr);
+  }
+
+  .items{
+    border: 1px solid black;
+    padding: 1rem;
+    margin: 1rem;
   }
   
 </style>
